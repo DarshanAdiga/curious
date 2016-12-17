3b5dae08264fa2f2e88adc2a7a439b6290cfc43e
Darshan Adiga <adigadarshan@gmail.com>
2016-12-18 01:08:21 +0530

    working classifier!

--- trainer.py
+++ trainer.py
@@ -3,22 +3,42 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn import svm
 
-with open("res/sample_train.csv", 'r') as train_file:
+with open("res/train.csv", 'r') as train_file:
     reader = csv.reader(train_file)
-    print(next(reader))
+    #print(next(reader))
     cats = []
     comments = []
     for line in reader:
         cats.append(line[0])
         comments.append(line[2])
-        print(line[0], ':', line[2])
+        #print(line[0], ':', line[2])
 
-    # count_vect = CountVectorizer()
-    # train_counts = count_vect.fit_transform(comments)
+    #count_vect = CountVectorizer()
+    #train_counts = count_vect.fit_transform(comments)
     # tfidf_transformer = TfidfTransformer()
     # train_tfidf = tfidf_transformer.fit_transform(train_counts)
     # print(train_tfidf[0, 0])
 
-    clf = svm.SVC()
-    print(comments)
-    clf.fit(np.array(cats), np.array(comments))
+    from sklearn.pipeline import Pipeline
+    from sklearn.naive_bayes import MultinomialNB
+    examples = ['I will kill you']
+
+    pipeline = Pipeline([
+        ('vectorizer', CountVectorizer(ngram_range=(1, 2))),
+        ('classifier', MultinomialNB())])
+    pipeline.fit(comments, cats)
+    predictions = pipeline.predict(examples)
+
+    # print(train_counts)
+    #classifier = MultinomialNB()
+    #classifier.fit(train_counts, cats)
+
+    #example_counts = count_vect.transform(examples)
+    #predictions = classifier.predict(example_counts)
+    #print(predictions)
+    if '1' in predictions:
+        print(examples)
+        print("Abusive")
+    else:
+        print(examples)
+        print("NonAbusive")
